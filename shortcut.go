fc9275e870070f28f36482fd3f0dbce63aefd5ad
cmstar <cosmo_sei@hotmail.com>
2021-11-21 08:59:14 +0000

    Fix bad example codes in the comments.

--- shortcut.go
+++ shortcut.go
@@ -9,33 +9,33 @@ import (
 
 var defaultConv = new(Conv)
 
-// ConvertType is equivalent to Conv{}.ConvertType() .
+// ConvertType is equivalent to new(Conv).ConvertType() .
 func ConvertType(src interface{}, dstTyp reflect.Type) (interface{}, error) {
 	return defaultConv.ConvertType(src, dstTyp)
 }
 
-// Convert is equivalent to Conv{}.Convert() .
+// Convert is equivalent to new(Conv).Convert() .
 func Convert(src interface{}, dstPtr interface{}) error {
 	return defaultConv.Convert(src, dstPtr)
 }
 
 // Bool converts the given value to the corresponding value of bool.
 // The value must be simple, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.SimpleToBool(v) .
+// It is equivalent to new(Conv).SimpleToBool(v) .
 func Bool(v interface{}) (bool, error) {
 	return defaultConv.SimpleToBool(v)
 }
 
 // String converts the given value to the corresponding value of string.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.SimpleToString(v) .
+// It is equivalent to new(Conv).SimpleToString(v) .
 func String(v interface{}) (string, error) {
 	return defaultConv.SimpleToString(v)
 }
 
 // Int converts the given value to the corresponding value of int.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(int(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(int(0))) .
 func Int(v interface{}) (int, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Int)
 	if err != nil {
@@ -46,7 +46,7 @@ func Int(v interface{}) (int, error) {
 
 // Int64 converts the given value to the corresponding value of int64.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(int64(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(int64(0))) .
 func Int64(v interface{}) (int64, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Int64)
 	if err != nil {
@@ -57,7 +57,7 @@ func Int64(v interface{}) (int64, error) {
 
 // Int32 converts the given value to the corresponding value of int32.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(int32(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(int32(0))) .
 func Int32(v interface{}) (int32, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Int32)
 	if err != nil {
@@ -68,7 +68,7 @@ func Int32(v interface{}) (int32, error) {
 
 // int16 converts the given value to the corresponding value of int16.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(int16(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(int16(0))) .
 func Int16(v interface{}) (int16, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Int16)
 	if err != nil {
@@ -79,7 +79,7 @@ func Int16(v interface{}) (int16, error) {
 
 // Int8 converts the given value to the corresponding value of int8.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(int8(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(int8(0))) .
 func Int8(v interface{}) (int8, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Int8)
 	if err != nil {
@@ -90,7 +90,7 @@ func Int8(v interface{}) (int8, error) {
 
 // Uint converts the given value to the corresponding value of uint.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(uint(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(uint(0))) .
 func Uint(v interface{}) (uint, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Uint)
 	if err != nil {
@@ -101,7 +101,7 @@ func Uint(v interface{}) (uint, error) {
 
 // Uint64 converts the given value to the corresponding value of uint64.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(uint(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(uint(0))) .
 func Uint64(v interface{}) (uint64, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Uint64)
 	if err != nil {
@@ -112,7 +112,7 @@ func Uint64(v interface{}) (uint64, error) {
 
 // Uint32 converts the given value to the corresponding value of uint32.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(uint(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(uint(0))) .
 func Uint32(v interface{}) (uint32, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Uint32)
 	if err != nil {
@@ -123,7 +123,7 @@ func Uint32(v interface{}) (uint32, error) {
 
 // Uint16 converts the given value to the corresponding value of uint16.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(uint(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(uint(0))) .
 func Uint16(v interface{}) (uint16, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Uint16)
 	if err != nil {
@@ -134,7 +134,7 @@ func Uint16(v interface{}) (uint16, error) {
 
 // Uint8 converts the given value to the corresponding value of uint8.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(uint(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(uint(0))) .
 func Uint8(v interface{}) (uint8, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Uint8)
 	if err != nil {
@@ -145,7 +145,7 @@ func Uint8(v interface{}) (uint8, error) {
 
 // Float64 converts the given value to the corresponding value of float64.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(float64(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(float64(0))) .
 func Float64(v interface{}) (float64, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Float64)
 	if err != nil {
@@ -156,7 +156,7 @@ func Float64(v interface{}) (float64, error) {
 
 // Float32 converts the given value to the corresponding value of float32.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(float32(0))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(float32(0))) .
 func Float32(v interface{}) (float32, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Float32)
 	if err != nil {
@@ -167,7 +167,7 @@ func Float32(v interface{}) (float32, error) {
 
 // Complex128 converts the given value to the corresponding value of complex128.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(complex128(0+0i))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(complex128(0+0i))) .
 func Complex128(v interface{}) (complex128, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Complex128)
 	if err != nil {
@@ -178,7 +178,7 @@ func Complex128(v interface{}) (complex128, error) {
 
 // Complex64 converts the given value to the corresponding value of complex64.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.Convert(v, reflect.TypeOf(complex64(0+0i))) .
+// It is equivalent to new(Conv).Convert(v, reflect.TypeOf(complex64(0+0i))) .
 func Complex64(v interface{}) (complex64, error) {
 	res, err := defaultConv.simpleToPrimitive(v, reflect.Complex64)
 	if err != nil {
@@ -189,7 +189,7 @@ func Complex64(v interface{}) (complex64, error) {
 
 // Time converts the given value to the corresponding value of time.Time.
 // The value must be a simple type, for which IsSimpleType() returns true.
-// It is equivalent to Conv{}.SimpleToSimple(v, reflect.TypeOf(time.Time{})) .
+// It is equivalent to new(Conv).SimpleToSimple(v, reflect.TypeOf(time.Time{})) .
 func Time(v interface{}) (time.Time, error) {
 	res, err := defaultConv.SimpleToSimple(v, typTime)
 	if err != nil {
@@ -198,12 +198,12 @@ func Time(v interface{}) (time.Time, error) {
 	return res.(time.Time), nil
 }
 
-// MapToStruct is equivalent to Conv{}.MapToStruct() .
+// MapToStruct is equivalent to new(Conv).MapToStruct() .
 func MapToStruct(m map[string]interface{}, dstTyp reflect.Type) (interface{}, error) {
 	return defaultConv.MapToStruct(m, dstTyp)
 }
 
-// StructToMap is equivalent to Conv{}.StructToMap() .
+// StructToMap is equivalent to new(Conv).StructToMap() .
 func StructToMap(v interface{}) (map[string]interface{}, error) {
 	return defaultConv.StructToMap(v)
 }
