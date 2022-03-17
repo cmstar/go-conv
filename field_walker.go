41a081c2d5fcc01a57cc9dcc97f20e0af766db31
cmstar <cosmo_sei@hotmail.com>
2022-03-17 22:57:07 +0800

    Add fieldWalker to traverse struct fields.

---
+++ field_walker.go
@@ -0,0 +1,265 @@
+package conv
+
+import (
+	"reflect"
+	"sync"
+)
+
+var fieldWalkerCache sync.Map
+
+// fieldWalker is used to traverse all field of a struct.
+//
+// The traverse will go into each level of embedded and untagged structs. Unexported fields are ignored.
+// It reads fields in this order:
+//   - Tagged fields.
+//   - Non-embedded struct or non-struct fields.
+//   - Fields of embedded struct, recursively.
+//
+// e.g. without tagName
+//   type Ec struct {
+//       D int
+//   }
+//   type Eb struct {
+//       B int // hided by T.B
+//       Ec    // hided by T.Ec.D
+//       C int
+//   }
+//   type T struct {
+//       A  int
+//       Eb
+//       B  string // hides Eb.B
+//       Ec
+//   }
+// The traverse order is:
+//   PATH     INDEX
+//   A        {0}
+//   B        {2}
+//   Eb.C     {1, 2}
+//   Ec.D     {3, 0}
+//
+// e.g. with tagName="json"
+//   type A struct {
+//     A int
+//     X int  // hided by T.B
+//   }
+//   type B struct {
+//     B1 int // absent
+//     B2 int // absent
+//   }
+//   type T struct {
+//     A
+//     B `json:"X"` // hides B.X, the traverse will not go into the field
+//   }
+// The traverse order is:
+//   PATH     INDEX    TAG
+//   B        {1}      X
+//   A.A      {0, 0}
+//
+type fieldWalker struct {
+	typ     reflect.Type
+	tagName string
+	mu      sync.Mutex
+	fields  []fieldInfo
+}
+
+type fieldInfo struct {
+	reflect.StructField
+
+	// If the field is a field of am embedded and untagged field which type is struct,
+	// the path is a dot-splitted string like A.B.C; otherwise it's equal to F.Name.
+	Path string
+
+	// The tag value of the field.
+	Tag string
+}
+
+func newFieldWalker(typ reflect.Type, tagName string) *fieldWalker {
+	type key struct {
+		reflect.Type
+		string
+	}
+	v, _ := fieldWalkerCache.LoadOrStore(key{typ, tagName}, &fieldWalker{
+		typ:     typ,
+		tagName: tagName,
+	})
+	return v.(*fieldWalker)
+}
+
+// WalkFields walks through fields of the given type of struct (or pointer) with a breadth-first traverse.
+// Each field will be send to the callback function. If the function returns false, the traverse stops.
+func (walker *fieldWalker) WalkFields(callback func(fieldInfo) bool) {
+	if walker.fields == nil {
+		walker.initFields()
+	}
+
+	for _, fieldInfo := range walker.fields {
+		if !callback(fieldInfo) {
+			break
+		}
+	}
+}
+
+// WalkValues is like WalkFields(), but walks through all field values.
+//
+// If a struct is embedded as a pointer, and the value is nil, the field is ignored.
+// If the given value is nil, the traverse stops with no callback.
+//
+func (walker *fieldWalker) WalkValues(value reflect.Value, callback func(fieldInfo, reflect.Value) bool) {
+	if walker.fields == nil {
+		walker.initFields()
+	}
+
+	// Try extract the underlying type of a pointer, stop on nil.
+	if value.Kind() == reflect.Ptr {
+		if value.IsNil() {
+			return
+		}
+
+		for {
+			value = value.Elem()
+			if value.Kind() != reflect.Ptr {
+				break
+			}
+		}
+	}
+
+	for _, fieldInfo := range walker.fields {
+		index := fieldInfo.Index
+		embedded := fieldInfo.Tag == "" && len(index) > 1
+
+		v := value
+		for i := 0; i < len(index); i++ {
+			v = v.Field(index[i])
+
+			if embedded {
+				for v.Kind() == reflect.Ptr {
+					if v.IsNil() {
+						goto next
+					}
+
+					v = v.Elem()
+				}
+			}
+		}
+
+		if !callback(fieldInfo, v) {
+			break
+		}
+
+	next:
+	}
+}
+
+func (walker *fieldWalker) initFields() {
+	walker.mu.Lock()
+	defer walker.mu.Unlock()
+
+	// Double-lock checking.
+	if walker.fields != nil {
+		return
+	}
+
+	fields := make([]fieldInfo, 0)
+	visited := make(map[string]struct{})
+
+	type fieldBuf struct {
+		Index []int        // If the current field is an embedded field, stores the field index sequence.
+		Path  string       // The field path, splitted by dots.
+		Type  reflect.Type // The type of the current field.
+	}
+
+	// Dequeue and traverse the first element, enqueue the types of embedded structs, then return then new q.
+	traverseOne := func(q []fieldBuf) []fieldBuf {
+		buf, q := q[0], q[1:] // Dequeue.
+		num := buf.Type.NumField()
+		tagged := make([]bool, num)
+
+		// Firstly read all tagged fields.
+		if walker.tagName != "" {
+			for i := 0; i < num; i++ {
+				f := buf.Type.Field(i)
+
+				// Ignore unexported fields. The document of PkgPath field says:
+				// PkgPath is the package path that qualifies a lower case (unexported)
+				// field name. It is empty for upper case (exported) field names.
+				if len(f.PkgPath) > 0 {
+					continue
+				}
+
+				tag := f.Tag.Get(walker.tagName)
+				if tag == "" {
+					continue
+				}
+
+				tagged[i] = true
+				visited[tag] = struct{}{}
+
+				fields = append(fields, fieldInfo{
+					StructField: f,
+					Path:        f.Name,
+					Tag:         tag,
+				})
+			}
+		}
+
+		// Read untagged fields.
+		for i := 0; i < num; i++ {
+			if tagged[i] {
+				continue
+			}
+
+			f := buf.Type.Field(i)
+			if len(f.PkgPath) > 0 {
+				continue
+			}
+
+			if _, ok := visited[f.Name]; ok {
+				continue
+			}
+
+			// Build the index sequence and path.
+			f.Index = append(buf.Index, f.Index...)
+			path := buf.Path
+			if path != "" {
+				path += "."
+			}
+			path += f.Name
+
+			if f.Anonymous {
+				// Try to extract the underlying type of a pointer.
+				ft := f.Type
+				for ft.Kind() == reflect.Ptr {
+					ft = ft.Elem()
+				}
+
+				// In a breadth-first traversal, the traverse of the embedded struct should be delayed.
+				if ft.Kind() == reflect.Struct {
+					q = append(q, fieldBuf{f.Index, path, ft})
+					continue
+				}
+			}
+
+			visited[f.Name] = struct{}{}
+			fields = append(fields, fieldInfo{
+				StructField: f,
+				Path:        path,
+			})
+		}
+		return q
+	}
+
+	typ := walker.typ
+	for typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+
+	q := []fieldBuf{{Type: typ}}
+	for {
+		q = traverseOne(q)
+		if len(q) == 0 {
+			break
+		}
+	}
+
+	walker.fields = fields
+}
---
+++ field_walker_test.go
@@ -0,0 +1,215 @@
+package conv
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_fieldWalker_WalkFields(t *testing.T) {
+	type want struct {
+		Name  string
+		Path  string
+		Index []int
+		Tag   string
+	}
+
+	check := func(t *testing.T, walker *fieldWalker, ws []want) {
+		i := 0
+		walker.WalkFields(func(f fieldInfo) bool {
+			if i > len(ws)-1 {
+				t.Fatalf("too many fields, index: %d", i)
+			}
+
+			w := ws[i]
+			if f.Name != w.Name {
+				t.Fatalf("want name %s, got %s", w.Name, f.Name)
+			}
+
+			if f.Path != w.Path {
+				t.Fatalf("want path %s, got %s", w.Path, f.Path)
+			}
+
+			if !reflect.DeepEqual(f.Index, w.Index) {
+				t.Fatalf("%s: want index %v, got %v", w.Name, w.Index, f.Index)
+			}
+
+			if f.Tag != w.Tag {
+				t.Fatalf("want tag %s, got %s", w.Tag, f.Tag)
+			}
+
+			i++
+			return true
+		})
+
+		if i != len(ws) {
+			t.Fatalf("not enough fields")
+		}
+	}
+
+	t.Run("empty", func(t *testing.T) {
+		walker := newFieldWalker(reflect.TypeOf(struct{}{}), "")
+		count := 0
+		walker.WalkFields(func(fi fieldInfo) bool {
+			count++
+			return true
+		})
+
+		if count != 0 {
+			t.FailNow()
+		}
+	})
+
+	t.Run("top2", func(t *testing.T) {
+		type s struct{ X, Y, Z int }
+		walker := newFieldWalker(reflect.TypeOf(s{}), "")
+		count := 0
+		walker.WalkFields(func(fi fieldInfo) bool {
+			count++
+			return count < 2
+		})
+
+		if count != 2 {
+			t.FailNow()
+		}
+	})
+
+	t.Run("without-tag", func(t *testing.T) {
+		type Ec struct {
+			D int
+			x int
+		}
+		type Eb struct {
+			B  int // hided by T.B
+			Ec     // hided by T.Ec.D
+			C  int
+		}
+		type T struct {
+			A  int
+			Eb `conv:"X"` // the tag will not be processed
+			B  string     // hides Eb.B
+			Ec
+		}
+		walker := newFieldWalker(reflect.TypeOf(T{}), "")
+		check(t, walker, []want{
+			{"A", "A", []int{0}, ""},
+			{"B", "B", []int{2}, ""},
+			{"C", "Eb.C", []int{1, 2}, ""},
+			{"D", "Ec.D", []int{3, 0}, ""},
+		})
+	})
+
+	t.Run("with-tag", func(t *testing.T) {
+		type A struct {
+			A int
+			X int // hided by T.B
+		}
+		type B struct {
+			B1 int // absent
+			B2 int // absent
+		}
+		type T struct {
+			*A
+			s int     `c:"V"`
+			B `c:"X"` // hides B.X, the traverse will not go into the field
+		}
+		walker := newFieldWalker(reflect.TypeOf(T{}), "c")
+		check(t, walker, []want{
+			{"B", "B", []int{2}, "X"},
+			{"A", "A.A", []int{0, 0}, ""},
+		})
+	})
+}
+
+func Test_fieldWalker_WalkValues(t *testing.T) {
+	type want struct {
+		Value int // 0 if the field isn't int.
+		Path  string
+	}
+
+	check := func(t *testing.T, walker *fieldWalker, val reflect.Value, ws []want) {
+		i := 0
+		walker.WalkValues(val, func(f fieldInfo, v reflect.Value) bool {
+			if i > len(ws)-1 {
+				t.Errorf("too many fields, index: %d", i)
+			}
+
+			w := ws[i]
+			if v.Kind() == reflect.Int && v.Interface() != w.Value {
+				t.Errorf("index %d %s, want value %v, got %v", i, f.Name, w.Value, v.Interface())
+			}
+
+			if f.Path != w.Path {
+				t.Errorf("want path %s, got %s", w.Path, f.Path)
+			}
+
+			i++
+			return true
+		})
+
+		if i != len(ws) {
+			t.Fatalf("not enough fields")
+		}
+	}
+
+	t.Run("nil", func(t *testing.T) {
+		var a *struct{}
+		walker := newFieldWalker(reflect.TypeOf(a), "")
+		walker.WalkValues(reflect.ValueOf(a), func(fi fieldInfo, v reflect.Value) bool {
+			t.FailNow()
+			return true
+		})
+	})
+
+	t.Run("top1", func(t *testing.T) {
+		var a struct{ X, Y int }
+		walker := newFieldWalker(reflect.TypeOf(a), "")
+
+		count := 0
+		walker.WalkValues(reflect.ValueOf(a), func(fi fieldInfo, v reflect.Value) bool {
+			count++
+			return count < 1
+		})
+
+		if count != 1 {
+			t.FailNow()
+		}
+	})
+
+	t.Run("all", func(t *testing.T) {
+		type A struct {
+			A1 int
+			A2 int
+			x  int
+		}
+		type B struct {
+			A  // hided by C.A
+			B1 int
+			A1 int // hided by C.A.A1
+		}
+		type C struct {
+			C1 int
+			C2 int
+			*A // nil pointer will be ignored, hides B.A
+			*B
+			D struct{ X, Y int }
+		}
+
+		c := &C{
+			C1: 1,
+			C2: 2,
+			B: &B{
+				A:  A{100, 200, 300},
+				B1: 10,
+				A1: 20, // hides A.A1
+			},
+		}
+
+		walker := newFieldWalker(reflect.TypeOf(c), "")
+		check(t, walker, reflect.ValueOf(c), []want{
+			{1, "C1"},
+			{2, "C2"},
+			{0, "D"},
+			{10, "B.B1"},
+		})
+	})
+}
